bec35e272ac1deaff0d7775d14a5588df28d8927
busson1 <busson1@tritons.iowacentral.edu>
2024-08-29 10:54:11 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+    Jake Busson
+    Computer Science
+    Due: Sept. 3, 2024
+    Lab Name: Lab 1 Intoduction to C++ Programing
+    Lab Description: This Lab 1 and our first project using the C++ programming software. I got a little stuck to start but figured out how to clone Mr. Busch's lab and then I was set for filling in my lines of code.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Carolina Blue"<< std::endl;
+    return 0;
+}
